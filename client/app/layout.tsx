c61e33d7f2fe3ec1b36224c1bf72bb395196436b
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2025-04-20 05:05:25 +0000

    Added Latin subset to fira Code font

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Fira_Code } from "next/font/google";
 import "./globals.css";
 
-const fira_code = Fira_Code();
+const fira_code = Fira_Code({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "text-to-SQL",
